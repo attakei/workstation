91e72f4b91a46236085d3039962f62a98182ea83
Kazuya Takei <attakei@gmail.com>
2019-11-16 21:55:23 +0900

    Add path for zsh

---
+++ playbook/files/zsh/env.zsh
@@ -0,0 +1,16 @@
+_os=`uname 2> /dev/null`
+
+# Android Studio (SDK management)
+export ANDROID_SDK_ROOT="$HOME/"`if [ $_os = 'Darwin' ] ; then echo Library; fi`"Android/sdk"
+
+# Paths
+python_user_base=`python -m site --user-base`/bin
+
+path=(
+    $python_user_base/bin(N-)
+    $HOME/.local/bin(N-)
+    $HOME/.local/node_modules/.bin(N-)
+    $ANDROID_SDK_ROOT/platform-tools(N-)
+    $ANDROID_SDK_ROOT/tools(N-)
+    $path
+)
