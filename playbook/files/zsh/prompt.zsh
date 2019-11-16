7da30183c81d380a06afe92141f45d31e8b87130
Kazuya Takei <attakei@gmail.com>
2019-11-16 23:54:39 +0900

    Cnahge theme by OMZ

--- playbook/files/zsh/prompt.zsh
+++ playbook/files/zsh/prompt.zsh
@@ -1,23 +1,8 @@
-function prompt_cwd {
-  echo '%F{5}%~%f'
-  return
-}
-
-function prompt_git {
-  if [ ! -e './.git' ] ; then
-    return
-  fi
-  prompt_str=`git branch| grep '*'| awk '{print $2}'`
-  color=2
-  diff_cnt=`git status -s | wc -l`
-  if [ $diff_cnt -gt 0 ] ; then
-    color=1
-    prompt_str="${prompt_str} + ${diff_cnt}"
-  fi
-  echo "%F{$color}<${prompt_str}>%f"
-}
-
+# --------------------------------------
+# Prompt configuration
+# --------------------------------------
 setopt prompt_subst
 
-PROMPT='`prompt_cwd` `prompt_git`
-$ '
+zplugin snippet OMZ::themes/sunrise.zsh-theme
+PROMPT='%B%F{5}$PROMPTPREFIX %2~%f $(custom_git_prompt)
+%B»%b '
--- playbook/files/zsh/zplugin.zsh
+++ playbook/files/zsh/zplugin.zsh
@@ -1,6 +1,15 @@
+# --------------------------------------
+# Zplugin plugin using
+# --------------------------------------
 zplugin light "b4b4r07/enhancd"
 zplugin light "zsh-users/zsh-completions"
 zplugin light "zsh-users/zsh-syntax-highlighting"
 
+# For using fzf
+zplugin ice from"gh-r" as"command"
 zplugin load junegunn/fzf-bin
 
+# For using OMZ theme
+zplugin snippet OMZ::lib/git.zsh
+zplugin snippet OMZ::plugins/git/git.plugin.zsh
+zplugin cdclear -q # <- forget completions provided up to this moment
