c1f166f0baedf12589e757e771418013cffa2567
Kazuya Takei <attakei@gmail.com>
2019-11-18 16:29:39 +0900

    Fix path configuration for Python3

--- files/zsh/env.zsh
+++ files/zsh/env.zsh
@@ -4,13 +4,13 @@ _os=`uname 2> /dev/null`
 export ANDROID_SDK_ROOT="$HOME/"`if [ $_os = 'Darwin' ] ; then echo Library; fi`"Android/sdk"
 
 # Paths
-python_user_base=`python -m site --user-base`/bin
+python_user_bin=`python3 -m site --user-base`/bin
 
 path=(
-    $python_user_base/bin(N-)
-    $HOME/.local/bin(N-)
-    $HOME/.local/node_modules/.bin(N-)
-    $ANDROID_SDK_ROOT/platform-tools(N-)
-    $ANDROID_SDK_ROOT/tools(N-)
+    $python_user_bin(N-/)
+    $HOME/.local/bin(N-/)
+    $HOME/.local/node_modules/.bin(N-/)
+    $ANDROID_SDK_ROOT/platform-tools(N-/)
+    $ANDROID_SDK_ROOT/tools(N-/)
     $path
 )
