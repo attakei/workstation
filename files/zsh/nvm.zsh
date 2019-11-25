df75c089bc0057cc80be5aa9bfe70a28d41d2346
Kazuya Takei <attakei@gmail.com>
2019-11-25 13:46:16 +0900

    NVM for macOS

--- files/zsh/nvm.zsh
+++ files/zsh/nvm.zsh
@@ -1,2 +1,13 @@
-# nvm
-source /usr/share/nvm/init-nvm.sh
+export NVM_DIR="$HOME/.nvm"
+
+# For Archlinux
+if [ -s "/usr/share/nvm/init-nvm.sh" ] ; then 
+    source /usr/share/nvm/init-nvm.sh
+fi
+# For macOS
+if [ -s "/usr/local/opt/nvm/nvm.sh" ] ; then 
+    source "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+fi
+if [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] ; then
+    source "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+fi
