8c4a8090839307a88483bfcb8bd61f9fb31684ed
Kazuya Takei <attakei@gmail.com>
2019-11-25 23:41:45 +0900

    Fix for flake8

--- playbook/lookup_plugins/dirglob.py
+++ playbook/lookup_plugins/dirglob.py
@@ -9,7 +9,7 @@ import os
 import glob
 
 from ansible.plugins.lookup import LookupBase
-from ansible.errors import AnsibleFileNotFound
+from ansible.errors import AnsibleFileNotFound  # noqa
 from ansible.module_utils._text import to_bytes, to_text
 
 
@@ -18,10 +18,16 @@ class LookupModule(LookupBase):
     def run(self, terms, variables=None, **kwargs):
 
         ret = []
+        errors_ = 'surrogate_or_strict'
         for term in terms:
             term_file = os.path.basename(term)
-            dwimmed_path = self.find_file_in_search_path(variables, 'files', os.path.dirname(term))
+            dwimmed_path = self.find_file_in_search_path(
+                variables, 'files', os.path.dirname(term))
             if dwimmed_path:
-                globbed = glob.glob(to_bytes(os.path.join(dwimmed_path, term_file), errors='surrogate_or_strict'))
-                ret.extend(to_text(g, errors='surrogate_or_strict') for g in globbed if os.path.isdir(g))
+                globbed = glob.glob(to_bytes(
+                    os.path.join(dwimmed_path, term_file), errors=errors_))
+                ret.extend(
+                    to_text(g, errors=errors_)
+                    for g in globbed if os.path.isdir(g)
+                )
         return ret
