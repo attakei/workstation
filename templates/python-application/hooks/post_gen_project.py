8d339ec1907d37b0d485869d10f550840231cf09
Kazuya Takei <myself@attakei.net>
2020-03-05 20:57:55 +0900

    Fix hook-command

--- templates/python-application/hooks/post_gen_project.py
+++ templates/python-application/hooks/post_gen_project.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
 from subprocess import Popen
 
 
-Popen("poetry install".split()).communicate()
-Popen("git init .".split()).communicate()
-Popen("git add .".split()).communicate()
-Popen("git commit -m 'Initial commit from cookiecutter'".split()).communicate()
+Popen(["poetry", "install"]).communicate()
+Popen(["git", "init", "."]).communicate()
+Popen(["git", "add", "."]).communicate()
+Popen(["git", "commit", "-m", "'Initial commit from cookiecutter'"]).communicate()
