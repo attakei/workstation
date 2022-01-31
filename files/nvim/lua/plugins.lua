9649e88655fd0dd3e0f320a6b73050c38d1b21e2
Kazuya Takei <myself@attakei.net>
2022-01-31 17:09:16 +0900

    feat(nvim): Set bootstrap for Packer

--- files/nvim/lua/plugins.lua
+++ files/nvim/lua/plugins.lua
@@ -1,6 +1,12 @@
 -- Configuration plugins and options
 vim.cmd [[packadd packer.nvim]]
 
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -24,4 +30,8 @@ return require('packer').startup(function(use)
     end
   }
   use 'editorconfig/editorconfig-vim'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
