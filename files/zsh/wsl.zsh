27de9252ca8512f9675ee5f21ce33554ee808cd0
Kazuya Takei <myself@attakei.net>
2022-07-05 00:42:57 +0900

    feat(zsh): Set default settings to use VcXsrv

--- files/zsh/wsl.zsh
+++ files/zsh/wsl.zsh
@@ -2,3 +2,7 @@
 if [ $(cat /proc/$PPID/cmdline|sed -e 's/\x0//g') = '/init' ] ; then
     cd $HOME
 fi
+
+# X and audio
+export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0 #GWSL
+export PULSE_SERVER=tcp:$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}') #GWSL
