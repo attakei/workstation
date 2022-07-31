7e288ea078c4e4ce8d0496fb91d7567d29dd3af8
Kazuya Takei <myself@attakei.net>
2022-07-31 16:44:45 +0900

    refactor(nvim):

--- files/nvim/lua/plugins.lua
+++ files/nvim/lua/plugins.lua
@@ -3,7 +3,14 @@ vim.cmd [[packadd packer.nvim]]
 
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  packer_bootstrap = vim.fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path,
+  })
 end
 
 
