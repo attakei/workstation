b02224c4ebea72f7585fa21163fe38a12f24e854
Kazuya Takei <myself@attakei.net>
2022-12-18 01:46:46 +0900

    fix(zsh): Change shortcut for avoid confict

--- files/zsh/plugins.zsh
+++ files/zsh/plugins.zsh
@@ -30,7 +30,7 @@ if [[ -n $ZENO_LOADED ]]; then
   bindkey ' '  zeno-auto-snippet
   bindkey '^m' zeno-auto-snippet-and-accept-line
   bindkey '^i' zeno-completion
-  bindkey '^/' zeno-ghq-cd
+  bindkey '^w' zeno-ghq-cd
   bindkey '^r' zeno-history-selection
   bindkey '^x' zeno-insert-snippet
 fi
