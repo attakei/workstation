6e846bab1c880871f2427ded6b0747822eebe34f
Kazuya Takei <myself@attakei.net>
2022-12-17 23:28:15 +0900

    fix(zsh): Change shortcut for avoid conflict

--- files/zsh/plugins.zsh
+++ files/zsh/plugins.zsh
@@ -30,7 +30,7 @@ if [[ -n $ZENO_LOADED ]]; then
   bindkey ' '  zeno-auto-snippet
   bindkey '^m' zeno-auto-snippet-and-accept-line
   bindkey '^i' zeno-completion
-  bindkey '^b' zeno-ghq-cd
+  bindkey '^/' zeno-ghq-cd
   bindkey '^r' zeno-history-selection
   bindkey '^x' zeno-insert-snippet
 fi
