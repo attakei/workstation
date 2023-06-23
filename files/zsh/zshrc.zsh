65e3d2235388242ad7ea32bf5a8b49810be56dd0
Kazuya Takei <myself@attakei.net>
2023-06-23 20:50:58 +0900

    feat: Simple setup tools for zsh

--- files/zsh/zshrc.zsh
+++ files/zsh/zshrc.zsh
@@ -5,13 +5,10 @@
 # Zinit
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 source "${ZINIT_HOME}/zinit.zsh"
-source "${HOME}/ws/files/zsh/zinit-plugins.sh"
-
-autoload -Uz _zinit
-(( ${+_comps} )) && _comps[zinit]=_zinit
+source "${HOME}/ws/files/zsh/zinit-plugins.zsh"
 
 # Custom settings for shell
-source "$HOME/ws/files/zsh/commands.sh"
+source "$HOME/ws/files/zsh/commands.zsh"
 @source prompt keybinds
 
 # Custom commands
@@ -32,3 +29,5 @@ if [ -e "$HOME/.zshrc.local" ] ; then
     source "$HOME/.zshrc.local"
 fi
 
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
