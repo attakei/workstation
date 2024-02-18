4fbc3c798c3cc874b252951d92dab69246e026f8
Kazuya Takei <myself@attakei.net>
2024-02-19 03:13:06 +0900

    fix: Simplify LSP

--- files/astronvim-user/init.lua
+++ files/astronvim-user/init.lua
@@ -2,5 +2,12 @@ return {
   colorscheme = "solarized",
   plugins = {
     "maxmx03/solarized.nvim",
+  },
+  lsp = {
+    servers = {
+      "esbonio",
+      "jedi_language_server",
+      "ruff_lsp",
+    }
   }
 }
